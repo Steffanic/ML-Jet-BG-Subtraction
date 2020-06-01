e2fa3b310bc120395a4e6b65f7d06937f5bb83d7
charles <chughe26@vols.utk.edu>
2020-06-01 02:51:29 -0400

    making sure merge and split work on ACF as intended

--- Analysis_Code/Grid Scripts/split_csv_files.sh
+++ Analysis_Code/Grid Scripts/split_csv_files.sh
@@ -7,10 +7,15 @@ splitCsv() {
         CHUNK=1000
     fi
 
-    split -dl $CHUNK --additional-suffix .csv $1 $1_MINI_
+    TEMP=$1
+    NAME=${TEMP%.csv}
+
+    echo $NAME
+
+    split -dl $CHUNK --additional-suffix .csv $1 $NAME"_MINI_"
 
     count=0
-    for i in $1"_MINI_"*csv; do
+    for i in $NAME"_MINI_"*csv; do
     if [ $count -gt 0 ]; then
       echo -e "$HEADER\n$(cat $i)" > $i
     fi
@@ -31,6 +36,8 @@ for(( w=0; w <=3; w++ ))
     for(( x=0; x <=4; x++ ))
       do  
 
+        echo "Rparam = ${JetR_array[$x]}, pThardmin = ${pThardmin_array[$w]}"
+
         DIR_STR=$(printf 'Rparam-=-%s-pThardmin-=%s' "${JetR_array[$x]}" "${pThardmin_array[$w]}")
 
         mkdir $MASTER_DIR_STR/$DIR_STR
@@ -39,7 +46,17 @@ for(( w=0; w <=3; w++ ))
      
         MERGE_CSVFILE_STR=$(printf 'merged-ML-output-LOWSTATS-Rparam-%s-pThardmin-%s.csv' "${JetR_array[$x]}" "${pThardmin_array[$w]}")
 
-        splitCsv "/lustre/haven/user/chughe26/scratch/jetbin1/$MERGE_CSVFILE_STR" 50000
+        cp "/lustre/haven/user/chughe26/scratch/jetbin1/$MERGE_CSVFILE_STR" .
+        splitCsv $MERGE_CSVFILE_STR 50000
+        rm -rf $MERGE_CSVFILE_STR
       done
   done
 
+cd /lustre/haven/user/chughe26/scratch/jetbin1
+echo "CLEANING UP THE EXCESS/INTERMEDIATE FILES NOW !!!"
+rm -rf 3*.csv
+rm -rf 3*.root
+echo "TAR-ING ALL THE REMANING ROOT FILES !"
+tar -zxvf ROOTFILES-LOWSTATS.tar.gz *.root
+echo "TAR-ING ALL THE REMAINING CSV FILES !"
+tar -zxvf CSVFILES-LOWSTATS.tar.gz *.csv LOWSTATS/
