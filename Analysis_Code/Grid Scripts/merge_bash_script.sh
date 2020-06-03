ffe9ef561e30aceb821fd60b65d1c72b268090b7
charles <chughe26@vols.utk.edu>
2020-06-03 00:06:17 -0400

    Making the merge script more general

--- Analysis_Code/Grid Scripts/merge_bash_script.sh
+++ Analysis_Code/Grid Scripts/merge_bash_script.sh
@@ -5,7 +5,7 @@
 pThardmin_array=(10.0 20.0 30.0 40.0)
 JetR_array=(0.2 0.3 0.4 0.5 0.6)
 
-for (( i=3657797; i<=3657823; i++ ))
+for (( i=3667261; i<=3667287; i++ ))
 do
    JOB_NUM=$((i))
    DIR_STR=$"/lustre/haven/user/chughe26/scratch/jetbin1/$JOB_NUM"".apollo-acf"
@@ -23,8 +23,8 @@ for(( w=0; w <=3; w++ ))
         ROOTFILE_STR=$(printf '%s-Rparam-=-%s-pThardmin-=-%s-.root' "$JOB_NUM" "${JetR_array[$x]}" "${pThardmin_array[$w]}")
         CSVFILE_STR=$(printf '%s-Rparam-=-%s-pThardmin-=-%s-.csv' "$JOB_NUM" "${JetR_array[$x]}" "${pThardmin_array[$w]}")
 
-        hadd $ROOTFILE_STR $DIR_STR/3*/*${JetR_array[$x]}*${pThardmin_array[$w]}*.root
-        cat $DIR_STR/3*/*${JetR_array[$x]}*${pThardmin_array[$w]}*.csv > $CSVFILE_STR
+        hadd $ROOTFILE_STR $DIR_STR/*/*${JetR_array[$x]}*${pThardmin_array[$w]}*.root
+        cat $DIR_STR/*/*${JetR_array[$x]}*${pThardmin_array[$w]}*.csv > $CSVFILE_STR
       done
   done
 
@@ -40,8 +40,8 @@ for(( r=0; r <=3; r++ ))
         MERGE_ROOTFILE_STR=$(printf 'merged-ML-output-LOWSTATS-Rparam-%s-pThardmin-%s.root' "${JetR_array[$s]}" "${pThardmin_array[$r]}")
         MERGE_CSVFILE_STR=$(printf 'merged-ML-output-LOWSTATS-Rparam-%s-pThardmin-%s.csv' "${JetR_array[$s]}" "${pThardmin_array[$r]}")       
 
-        hadd $MERGE_ROOTFILE_STR /lustre/haven/user/chughe26/scratch/jetbin1/3*${JetR_array[$s]}*${pThardmin_array[$r]}*.root
-        cat /lustre/haven/user/chughe26/scratch/jetbin1/3*${JetR_array[$s]}*${pThardmin_array[$r]}*.csv > $MERGE_CSVFILE_STR
+        hadd $MERGE_ROOTFILE_STR /lustre/haven/user/chughe26/scratch/jetbin1/*${JetR_array[$s]}*${pThardmin_array[$r]}*.root
+        cat /lustre/haven/user/chughe26/scratch/jetbin1/*${JetR_array[$s]}*${pThardmin_array[$r]}*.csv > $MERGE_CSVFILE_STR
       done
   done
 
