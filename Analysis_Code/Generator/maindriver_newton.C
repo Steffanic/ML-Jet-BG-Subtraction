3486d8984560d553103d57d9fcaceef4696d3ded
charles <chughe26@vols.utk.edu>
2020-06-11 16:06:25 -0400

    Adding capability for Perugia Tunes

--- Analysis_Code/Generator/maindriver_newton.C
+++ Analysis_Code/Generator/maindriver_newton.C
@@ -1,11 +1,13 @@
 //This file creates the interface b/w root and pythia libraries for macros you want to run in root that use
 //pythia libraries
 
-void maindriver_newton(Int_t nEvents = 100000000, Int_t jobID =39034, Int_t tune = 100, Double_t Jet_Radius = 0.4, Int_t HF = 0 , Double_t DCA = 1000 , Int_t cent_bin = 0 , Double_t pT_hard_min = 10., Int_t Num_BKGD_Files = 5 , Double_t constit_cut = 0.,  Bool_t Data = kTRUE, Bool_t RT_Stats = kFALSE , Bool_t GRID = kFALSE ){
+void maindriver_newton(Int_t nEvents = 100000000, Int_t jobID =39034, Int_t tune = 356, Double_t Jet_Radius = 0.4, Int_t HF = 0 , Double_t DCA = 1000 , Int_t cent_bin = 0 , Double_t pT_hard_min = 10., Int_t Num_BKGD_Files = 5 , Double_t constit_cut = 0.,  Bool_t Data = kTRUE, Bool_t RT_Stats = kFALSE , Bool_t GRID = kFALSE ){
 
 
 if( !GRID ){
 //________________load Pythia 6 and link to your library (libPythia6.s0)_________________________________________________//
+gSystem->Load("liblhapdf");  
+gSystem->Load("libpythia6_4_28");
 gSystem->Load("libEGPythia6");
 gSystem->Load("/home/alidock/ML/pythia6/libPythia6.so"); //change to your setup
 //gSystem->Load("/home/charles/Documents/alice_software/pythia6/libPythia6.so"); //change to your setup
@@ -17,6 +19,7 @@ else if( GRID){
 
 //____//below lines needed on ACF !!!!__________________________________________________________//
 
+  gSystem->Load("liblhapdf");  
   gSystem->Load("libpythia6_4_28");
   gSystem->Load("libEGPythia6");
   gSystem->Load("libEG");
