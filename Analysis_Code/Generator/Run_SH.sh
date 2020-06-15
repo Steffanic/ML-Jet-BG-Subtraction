8e576af92f45565bd5600dfcbfeaf8a0b454fc84
charles <chughe26@vols.utk.edu>
2020-06-14 21:30:21 -0400

    Latest Bump Engineering Scheme (tentative)

--- Analysis_Code/Generator/Run_SH.sh
+++ Analysis_Code/Generator/Run_SH.sh
@@ -9,7 +9,7 @@ for(( i=0; i <=3;  i++ ))
         var=$(printf '\npThardmin = %s, Rparam = %s\n' "${pThardmin_array[$i]}" "${JetR_array[$j]}")
         echo $var
         echo '________________________________________________________________________________________________'
-        root -b -l -q "maindriver_newton.C( 100000000, 39034, 356, ${JetR_array[$j]}, 0 , 1000 , 0 , ${pThardmin_array[$i]} , 1 , 0., kTRUE, kFALSE , kFALSE )"
+        root -b -l -q "maindriver_newton.C( 100000000, 39034, 356, ${JetR_array[$j]}, 0 , 1000 , 0 , ${pThardmin_array[$i]} , 1 , 0., kTRUE, kTRUE , kFALSE )"
       done
   done
 
