849ed191442bd77ab8670af8dd50df067f39fc43
Patrick Steffanic <psteffa1@acf-login8.acf.nics.utk.edu>
2020-12-18 09:08:57 -0500

    I set_array-ed the wrong thing

--- RFBDoSBC/modelEvaluation.py
+++ RFBDoSBC/modelEvaluation.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 from sklearn import tree
 import matplotlib.pyplot as plt
--- runSuite.sh
+++ runSuite.sh
@@ -1,4 +1,4 @@
-python3 RFBDoSBC 1 1000000000000
-python3 Plots/makePlots.py
-python3 Plots/plotFeatImp.py
-python3 Plots/plotPerfMetric.py
+python RFBDoSBC 1 100000
+python Plots/makePlots.py
+python Plots/plotFeatImp.py
+python Plots/plotPerfMetric.py
